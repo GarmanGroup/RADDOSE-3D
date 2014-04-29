91f607d5ef01699850ee501376e717a2ad2dfe14
Markus Gerstel <markus.gerstel@dtc.ox.ac.uk>
2014-04-29 12:05:37 +0100

    package info for s.r.r.server

---
+++ src/se/raddo/raddose3D/server/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * RADDOSE-3D server
+ * time- and space-resolved modelling of dose in macromolecular crystallography.
+ * 
+ * This is a sample implementation of a RADDOSE-3D server, regularly looking
+ * for new jobs to process from a database, starting RADDOSE-3D instances and
+ * writing the results back into the database.
+ * 
+ * Please cite
+ *  Zeldin, Gerstel, Garman. (2013). J. Appl. Cryst. 46, 1225-1230.
+ *  http://dx.doi.org/10.1107/S0021889813011461
+ */
+package se.raddo.raddose3D.server;
