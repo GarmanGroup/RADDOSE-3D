77764dd3bab4194a8149a67d369941cc6f84b1a2
Helen Ginn <helen.ginn@magd.ox.ac.uk>
2014-05-10 16:33:00 +0100

    CoefCalcTest

--- src/se/raddo/raddose3D/tests/CoefCalcTests.java
+++ src/se/raddo/raddose3D/tests/CoefCalcTests.java
@@ -31,4 +31,33 @@ public class CoefCalcTests {
 
     Assertion.equals(hydrogenOccurrence, oxygenOccurrence * 2, "O vs H");
   }
+
+  /**
+   * Test to make sure protein heavy atoms are added and
+   * multiplied by no. of monomers correctly
+   */
+  @Test
+  public void testCoefCalcHeavyProteinAtoms() {
+    List<String> emptyAtoms = new ArrayList<String>();
+    List<Double> emptyNumbers = new ArrayList<Double>();
+
+    List<String> atoms = new ArrayList<String>();
+    List<Double> numbers = new ArrayList<Double>();
+
+    atoms.add("zn");
+    numbers.add(Double.valueOf(2));
+
+    CoefCalcFromParams coefCalc = new CoefCalcFromParams(Double.valueOf(100.0),
+        Double.valueOf(100.0), Double.valueOf(100.0), Double.valueOf(90.0),
+        Double.valueOf(90.0), Double.valueOf(90.0), 24, 10, 0, 0, atoms,
+        numbers,
+        emptyAtoms, emptyNumbers, Double.valueOf(100.0));
+
+    Double zincOccurrence = Double.valueOf(coefCalc.getParser()
+        .findAtomWithName("ZN").getMacromolecularOccurrence());
+
+    Assertion.equals(zincOccurrence, 48, "Zn = 48");
+  }
+
+
 }
