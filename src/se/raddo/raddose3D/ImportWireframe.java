0c717782d722b4f89164ef58de90ccd1ac5b861c
Helen Ginn <helen.ginn@magd.ox.ac.uk>
2014-05-14 12:03:35 +0100

    ImportWireframe interface put in place.

---
+++ src/se/raddo/raddose3D/ImportWireframe.java
@@ -0,0 +1,24 @@
+package se.raddo.raddose3D;
+
+/**
+ * Interface for importing wireframe from various types of file
+ * formats containing vertex/index information.
+ * @author magd3052
+ *
+ */
+public interface ImportWireframe {
+  /**
+   * Returns array of three-dimensional vectors containing vertex
+   * information in crystal coordinates (dimensions in um).
+   * @return array of vertices
+   */
+  public double[][] getVertices();
+  
+  /**
+   * Returns connectivity of vertices starting from index 1 (not 0)
+   * in groups of three indices for each triangle.
+   * @return array of indices
+   */
+  public double[][] getIndices();
+  
+}
