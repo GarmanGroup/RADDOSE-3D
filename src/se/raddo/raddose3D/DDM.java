1d25393a9eceb7a83ba1b4f7a2c267478d7ef3c5
Jonny Brooks <jonathan.brooks-bartlett@dtc.ox.ac.uk>
2014-05-17 01:04:31 +0100

    Added comments to the DDM interface

--- src/se/raddo/raddose3D/DDM.java
+++ src/se/raddo/raddose3D/DDM.java
@@ -1,5 +1,16 @@
 package se.raddo.raddose3D;
 
+/**
+ * Describes the DDM classes. It contains a method that calculates the
+ * diffraction efficiency decay as a function of dose.
+ */
 public interface DDM {
+  /**
+   * Calculates the decay of diffraction efficiency as a function of dose
+   *
+   * @param dose the dose absorbed within a crystal voxel
+   *
+   * @return The Relative Diffraction Efficiency (RDE)
+   */
   public double calcDecay(double dose);
 }
