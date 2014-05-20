e885bc47595d103bcd8b135c2281b51bab03f129
Markus Gerstel <markus.gerstel@dtc.ox.ac.uk>
2014-05-20 10:30:53 +0100

    RDV2 test

--- src/se/raddo/raddose3D/tests/CoefCalcTests.java
+++ src/se/raddo/raddose3D/tests/CoefCalcTests.java
@@ -1,10 +1,15 @@
 package se.raddo.raddose3D.tests;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.testng.annotations.*;
 
+import se.raddo.raddose3D.Assertions;
+import se.raddo.raddose3D.Beam;
+import se.raddo.raddose3D.BeamTophat;
 import se.raddo.raddose3D.CoefCalcFromParams;
 
 /**
@@ -59,4 +64,42 @@ public class CoefCalcTests {
     Assertion.equals(zincOccurrence, 48, "Zn = 48");
   }
 
+  /**
+   * Run an actual scenario and compare to values obtained by RADDOSE2
+   */
+  @Test
+  public void testCoefCalcScenario() {
+    List<String> heavyProtAtomNames = new ArrayList<String>();
+    List<Double> heavyProtAtomNums = new ArrayList<Double>();
+
+    List<String> heavySolutionConcNames = new ArrayList<String>();
+    List<Double> heavySolutionConcNums = new ArrayList<Double>();
+
+    heavyProtAtomNames.add("S");
+    heavyProtAtomNums.add(10.0);
+
+    heavySolutionConcNames.add("Na");
+    heavySolutionConcNames.add("Cl");
+    heavySolutionConcNums.add(1200.);
+    heavySolutionConcNums.add(200.);
+
+    CoefCalcFromParams coefCalc = new CoefCalcFromParams(
+        79.2, 79.2, 38.1, 90.0, 90.0, 90.0, 8, 129, 0, 0,
+        heavyProtAtomNames, heavyProtAtomNums,
+        heavySolutionConcNames, heavySolutionConcNums, 0.);
+
+    Map<Object, Object> beamProperties = new HashMap<Object, Object>();
+    beamProperties.put(Beam.BEAM_COLL_H, 80.);
+    beamProperties.put(Beam.BEAM_COLL_V, 80.);
+    beamProperties.put(Beam.BEAM_FLUX, 9.281e8);
+    beamProperties.put(Beam.BEAM_ENERGY, 8.05);
+    Beam b = new BeamTophat(beamProperties);
+
+    coefCalc.updateCoefficients(null, b);
+
+    Assertion.equals(coefCalc.getAbsorptionCoefficient(), 1.04,
+        "Absorption Coefficient");
+    Assertion.equals(coefCalc.getAttenuationCoefficient(), 1.095,
+        "Attenuation Coefficient");
+  }
 }
