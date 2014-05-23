d0fd85fb9b15910b7b68fb4c37e4014dbe75d954
Jonny Brooks <jonathan.brooks-bartlett@dtc.ox.ac.uk>
2014-05-23 18:44:14 +0100

    Please check the WriterCompress class. There shouldn't be any changes

---
+++ src/se/raddo/raddose3D/WriterCompress.java
@@ -0,0 +1,38 @@
+package se.raddo.raddose3D;
+
+/**
+ * WriterCompress applies ZIP compression to any output, and passes it on to
+ * another Writer instance.
+ */
+public class WriterCompress implements Writer {
+  /** The Writer class that output should be directed to. */
+  private final Writer output;
+
+  /**
+   * Compress any output and send it on to another Writer instance.
+   * 
+   * @param w
+   *          Writer instance, where the compressed output stream should end up.
+   */
+  public WriterCompress(Writer w) {
+    output = w;
+  }
+
+  @Override
+  public void write(String s) {
+    // TODO Auto-generated method stub
+    output.write(s);
+  }
+
+  @Override
+  public void write(StringBuffer b) {
+    // TODO Auto-generated method stub
+    output.write(b);
+  }
+
+  @Override
+  public void close() {
+    // TODO Auto-generated method stub
+    output.close();
+  }
+}
