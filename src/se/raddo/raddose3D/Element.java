f6575766371ef0e0d9192072ad3c95c7e3de551c
JonnyCBB <jonathan.brooks-bartlett@dtc.ox.ac.uk>
2016-10-07 22:01:32 +0100

    add getter methods to get relevant element database info for Fluorescent escape calculation

--- src/se/raddo/raddose3D/Element.java
+++ src/se/raddo/raddose3D/Element.java
@@ -350,4 +350,142 @@ public class Element {
   public Double getAtomicWeightInGrams() {
     return getAtomicWeight() * ATOMIC_MASS_UNIT;
   }
+  
+  /**
+   * Return the K edge energy in keV of the element
+   * 
+   * @return
+   *         the K edge energy in keV
+   */
+  public Double getKEdge() {
+    return elementData.get(DatabaseFields.EDGE_K);
+  }
+  
+  /**
+   * Return the L1 edge energy in keV of the element
+   * 
+   * @return
+   *         the L1 edge energy in keV
+   */
+  public Double getL1Edge() {
+    return elementData.get(DatabaseFields.EDGE_L);
+  }
+  
+  /**
+   * Return the L2 edge energy in keV of the element
+   * 
+   * @return
+   *         the L2 edge energy in keV
+   */
+  public Double getL2Edge() {
+    return elementData.get(DatabaseFields.L2);
+  }
+  
+  /**
+   * Return the L3 edge energy in keV of the element
+   * 
+   * @return
+   *         the L3 edge energy in keV
+   */
+  public Double getL3Edge() {
+    return elementData.get(DatabaseFields.L3);
+  }
+  
+  /**
+   * Return the M1 edge energy in keV of the element
+   * 
+   * @return
+   *         the M1 edge energy in keV
+   */
+  public Double getM1Edge() {
+    return elementData.get(DatabaseFields.EDGE_M);
+  }
+  
+  /**
+   * Return the K edge ratio which is defined as the ratio
+   * of the K shell to L1 shell photoelectric cross sections.
+   * i.e. muK / muL1
+   * 
+   * @return
+   *         the K edge ratio
+   */
+  public Double getKEdgeRatio() {
+    return elementData.get(DatabaseFields.K_EDGE_RATIO);
+  }
+  
+  /**
+   * Return the L1 edge ratio which is defined as the ratio
+   * of the L1 shell to M shell photoelectric cross sections.
+   * i.e. muL1 / muM
+   * 
+   * @return
+   *         the L1 edge ratio
+   */
+  public Double getL1EdgeRatio() {
+    return elementData.get(DatabaseFields.L1_EDGE_RATIO);
+  }
+  
+  /**
+   * Return the L2 edge ratio which is defined as the ratio
+   * of the L2 shell to M shell photoelectric cross sections.
+   * i.e. muL2 / muM
+   * 
+   * @return
+   *         the L2 edge ratio
+   */
+  public Double getL2EdgeRatio() {
+    return elementData.get(DatabaseFields.L2_EDGE_RATIO);
+  }
+  
+  /**
+   * Return the L3 edge ratio which is defined as the ratio
+   * of the L3 shell to M shell photoelectric cross sections.
+   * i.e. muL3 / muM
+   * 
+   * @return
+   *         the L3 edge ratio
+   */
+  public Double getL3EdgeRatio() {
+    return elementData.get(DatabaseFields.L3_EDGE_RATIO);
+  }
+  
+  /**
+   * Return the K shell fluorescence yield of the atom
+   * 
+   * @return
+   *         the K shell fluorescence yield
+   */
+  public Double getKShellFluorescenceYield() {
+    return elementData.get(DatabaseFields.FLUORESCENCE_YIELD_K);
+  }
+  
+  /**
+   * Return the L1 shell fluorescence yield of the atom
+   * 
+   * @return
+   *         the L1 shell fluorescence yield
+   */
+  public Double getL1ShellFluorescenceYield() {
+    return elementData.get(DatabaseFields.FLUORESCENCE_YIELD_L1);
+  }
+  
+  /**
+   * Return the L2 shell fluorescence yield of the atom
+   * 
+   * @return
+   *         the L2 shell fluorescence yield
+   */
+  public Double getL2ShellFluorescenceYield() {
+    return elementData.get(DatabaseFields.FLUORESCENCE_YIELD_L2);
+  }
+  
+  /**
+   * Return the L3 shell fluorescence yield of the atom
+   * 
+   * @return
+   *         the L3 shell fluorescence yield
+   */
+  public Double getL3ShellFluorescenceYield() {
+    return elementData.get(DatabaseFields.FLUORESCENCE_YIELD_L3);
+  }
 }
