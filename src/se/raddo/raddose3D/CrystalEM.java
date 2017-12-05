c32e334fde8ed1621692495a36639a80f9476ae0
Joshua Dickerson <orie3208@bioch.ox.ac.uk>
2017-12-05 15:18:12 +0000

    things changed by themselves idk why

--- src/se/raddo/raddose3D/CrystalEM.java
+++ src/se/raddo/raddose3D/CrystalEM.java
@@ -9,14 +9,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.openqa.selenium.support.ui.ExpectedCondition;
+//import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.text.*;
-import java.math.*;
+//import java.math.*;
 
 public class CrystalEM extends Crystal {
 
